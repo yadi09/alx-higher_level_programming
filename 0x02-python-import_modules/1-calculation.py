338d95d19cc77151aca14827f29e016755b63d61
yadi09 <yadamzerterefe09@gmail.com>
2024-01-18 19:02:11 +0100

    Task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-print(a, "+", b, "=", add(a, b))
-print(a, "-", b, "=", sub(a, b))
-print(a, "*", b, "=", mul(a, b))
-print(a, "/", b, "=", div(a, b))
+print("{:d} + {:d} = {}".format(a, b, add(a, b)))
+print("{:d} - {:d} = {}".format(a, b, sub(a, b)))
+print("{:d} * {:d} = {}".format(a, b, mul(a, b)))
+print("{:d} / {:d} = {}".format(a, b, div(a, b)))
