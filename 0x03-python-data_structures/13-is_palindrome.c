2924ad60ee7c99ccf4085c70c85f160432267090
yadi09 <yadamzerterefe09@gmail.com>
2024-01-21 19:09:49 +0100

    Task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * is_palindrom - function
+ * @head: singly linked list
+ * Return: 0 or 1
+ **/
+
+int is_palindrome(listint_t **head)
+{
+  listint_t *first_node = *head;
+  listint_t *last_node = *head;
+  listint_t *length_node = *head;
+  int len = 0, i, j;
+  int l_len = 0;
+
+  while (length_node->next)
+    {
+      length_node = length_node->next;
+      len++;
+    }
+
+  for (i = 0; i < (l_len / 2); i++)
+    {
+      len = len - 1;
+      while (j < len)
+	{
+	  last_node = last_node->next;
+	  j++;
+	}
+      if (first_node->n != last_node->n)
+	{
+	  return (0);
+	}
+      first_node = first_node->next;
+    }
+
+  return (1);
+}
