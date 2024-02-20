a5b7409574c9d7ac13260cb3c3a9f6f5874d9d05
yadi09 <yadamzerterefe09@gmail.com>
2024-02-20 22:19:08 +0100

    Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that returns the JSON representation of an object (string)"""
+import json
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of my_obj"""
+    return json.dumps(my_obj, sort_keys=True)
