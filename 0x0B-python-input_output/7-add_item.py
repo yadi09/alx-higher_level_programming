7dd021b48ac92b22cc3ab37083d7b543afe3b694
yadi09 <yadamzerterefe09@gmail.com>
2024-02-20 23:34:35 +0100

    Task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import json
+from sys import argv
+import os.path
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+if os.path.isfile(filename):
+    py_list = load_from_json_file(filename)
+else:
+    py_list = []
+
+py_list.extend(argv[1:])
+save_to_json_file(py_list, filename)
