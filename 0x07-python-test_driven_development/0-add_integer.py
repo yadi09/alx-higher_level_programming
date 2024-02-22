e73e280a6511bf67d17bd2003fcfce36f3e036cf
yadi09 <yadamzerterefe09@gmail.com>
2024-02-22 08:21:13 +0100

    Task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+"""function that adds 2 integers."""
+
+
+def add_integer(a, b=98):
+    """function that adds 2 integers."""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
