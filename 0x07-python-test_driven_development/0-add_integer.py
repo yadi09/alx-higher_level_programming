82fc4e6eb20ed4c1edf8bbacf39df0318f94d008
yadi09 <yadamzerterefe09@gmail.com>
2024-02-22 08:24:32 +0100

    Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,14 @@
-"""function that adds 2 integers."""
+"""function that adds 2 integers.
+a and b must be integers or floats,
+otherwise raise a TypeError exception with the message
+"a must be an integer" or "b must be an integer" 
+"""
 
 
 def add_integer(a, b=98):
-    """function that adds 2 integers."""
+    """function that adds 2 integers.
+       return a + b
+    """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
