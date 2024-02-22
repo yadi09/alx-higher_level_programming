49489fd0520177b7a6ad708e2c73e2d8cebc1ee0
yadi09 <yadamzerterefe09@gmail.com>
2024-02-22 15:38:39 +0100

    Task 5

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,10 +7,26 @@ class TestMax_integer(unittest.TestCase):
     def test_empty_list(self):
         self.assertEqual(max_int(), None)
 
-    def test_int_list(self):
-        l = [10, 32, 1, 100, 0]
+    def test_max_end(self):
+        l = [10, 32, 1, 100]
         self.assertEqual(max_int(l), 100)
 
-    def test_float_list(self):
-        l = [10.34535, 2.234, 10.346]
-        self.assertEqual(max_int(l), 10.346)
+    def test_max_beginning(self):
+        l = [100, 23, 3, 0]
+        self.assertEqual(max_int(l), 100)
+
+    def test_max_middle(self):
+        l = [1, 23, 100, 2, 0]
+        self.assertEqual(max_int(l), 100)
+
+    def test_with_negative(self):
+        l = [1, -23, 100, 0]
+        self.assertEqual(max_int(l), 100)
+
+    def test_only_negative(self):
+        l = [-12, -43, -100, -1]
+        self.assertEqual(max_int(l), -1)
+
+    def test_one_element(self):
+        l = [10]
+        self.assertEqual(max_int(l), 10)
