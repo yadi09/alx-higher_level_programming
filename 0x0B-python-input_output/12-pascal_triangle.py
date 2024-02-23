cea590778f38db9ec854fb69bf9867e438810b0f
yadi09 <yadamzerterefe09@gmail.com>
2024-02-23 13:36:56 +0100

    Task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,6 +6,10 @@ representing the Pascal’s triangle of n:
 
 
 def pascal_triangle(n):
+    """
+    returns a list of lists of integers representing
+    the Pascal’s triangle of n
+    """
     pacl_trgl = [[1]]
     if n <= 0:
         return []
