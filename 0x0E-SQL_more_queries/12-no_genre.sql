2ab990f0a412eefd8df2ca6a18c562862afded51
yadi09 <yadamzerterefe09@gmail.com>
2024-03-28 12:19:39 +0100

    Task 12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,14 @@
+-- script that lists all shows contained,
+-- in hbtn_0d_tvshows without a genre linked.
+-- Each record should display:
+-- 	tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by:
+-- 	   tv_shows.title and tv_show_genres.genre_id
+
+
+
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id
