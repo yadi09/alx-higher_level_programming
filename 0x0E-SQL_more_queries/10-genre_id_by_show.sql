dc18cdb5d706346ea922e1cf25f26c2133225ef5
yadi09 <yadamzerterefe09@gmail.com>
2024-03-28 11:50:55 +0100

    Task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -7,6 +7,6 @@
 
 
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-JOIN tv_show_genres ON tv_show_genres.genre_id IS NOT NULL
+JOIN tv_show_genres ON tv_show_genres.show_id=tv_shows.id
 GROUP BY tv_shows.title, tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id
