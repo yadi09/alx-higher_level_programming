b60cb25fed5b9336826a60880e3e57ba65b3a59a
yadi09 <yadamzerterefe09@gmail.com>
2024-03-28 12:32:32 +0100

    Task 20

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,12 @@
+-- script that lists all genres in the hbtn_0d_tvshows_rate,
+-- 	  by their rating.
+
+
+
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings
+ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.id
+ORDER BY rating DESC;
