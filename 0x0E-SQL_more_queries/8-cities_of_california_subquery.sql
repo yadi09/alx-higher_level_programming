d635f94413f9ae7c8a6969dcdaa1412f30a4e975
yadi09 <yadamzerterefe09@gmail.com>
2024-03-28 04:11:15 +0100

    Task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,4 +2,4 @@
 -- that can be found in the database hbtn_0d_usa.
 
 
-SELECT states.id, cities.name FROM states, cities WHERE cities.id = state.state_id ORDER BY id;
+SELECT states.id, cities.name FROM states, cities WHERE states.id = 1 ORDER BY id;
