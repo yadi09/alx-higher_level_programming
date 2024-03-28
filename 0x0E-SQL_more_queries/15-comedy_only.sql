9a56d33eefd168658e86b00e558cb2527dcfca1d
yadi09 <yadamzerterefe09@gmail.com>
2024-03-28 12:25:28 +0100

    Task 15

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,12 @@
+-- script that lists all Comedy shows in the database
+-- The tv_genres table contains only one record,
+-- where name = Comedy (but the id can be different)
+
+
+SELECT tv_shows.title FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
