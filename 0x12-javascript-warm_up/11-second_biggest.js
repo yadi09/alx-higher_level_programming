e4e4393f757d1a9a03d78334868f671e14a80bca
yadi09 <yadamzerterefe09@gmail.com>
2024-04-23 01:46:45 +0100

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,11 +6,13 @@ let max2nd = 0;
 if (!process.argv[3]) {
   console.log(max);
 } else {
-  for (let i = 2; i < Number(process.argv.length); i++) {
+  for (let i = 2; i < process.argv.length; i++) {
     const num = Number(process.argv[i]);
     if (max <= num) {
       max2nd = max;
       max = num;
+    } else if (max2nd < num) {
+      max2nd = num;
     }
   }
   console.log(max2nd);
