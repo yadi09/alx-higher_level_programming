fe013a58aae0b2b495d83af348451eb656d0d782
Yadamzer <yadamzerterefe09@gmail.com>
2024-06-20 16:43:38 -0700

    Task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -10,10 +10,14 @@ class Rectangle {
 
   print () {
     if (this.width && this.height) {
+      let x = '';
+      let isFirstTime = false;
       for (let i = 0; i < this.height; i++) {
-        let x = '';
-        for (let j = 0; j < this.width; j++) {
-          x += 'x';
+        if (isFirstTime) {
+          for (let j = 0; j < this.width; j++) {
+            x += 'x';
+          }
+          isFirstTime = true;
         }
         console.log(x);
       }
