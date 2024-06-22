c0454b2b962834c542fa6e3000a5c12513fc5f4d
Yadamzer <yadamzerterefe09@gmail.com>
2024-06-22 14:46:58 -0700

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""a script that lists all states from the database hbtn_0e_0_usa:"""
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    "a script that lists all states from the database hbtn_0e_0_usa:"
     db = MySQLdb.connect(
         host="localhost",
         user=sys.argv[1],
