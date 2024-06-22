a8384f7afe696eceb5f8d0eae860dba781f4ed1f
Yadamzer <yadamzerterefe09@gmail.com>
2024-06-22 14:41:21 -0700

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
         host="localhost",
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        db=sys.argv[3],
+        port=3306
         )
 
     cursor = db.cursor()
