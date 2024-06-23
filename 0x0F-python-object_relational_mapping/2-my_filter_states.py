87238223be24b93d9e1121a78d7ed68eb2938461
Yadamzer <yadamzerterefe09@gmail.com>
2024-06-23 00:49:53 -0700

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """a script that takes in an argument and displays
-all values in the states table of hbtn_0e_0_usa 
+all values in the states table of hbtn_0e_0_usa
 where name matches the argument."""
 
 import MySQLdb
@@ -17,13 +17,12 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name='{:s}'".format(sys.argv[4])
-    print(query)
+    query = "SELECT * FROM states WHERE BINARY name='{:s}'".format(sys.argv[4])
     cursor.execute(query)
 
     result = cursor.fetchall()
     for row in result:
         print(row)
 
-    db.close()
     cursor.close()
+    db.close()
