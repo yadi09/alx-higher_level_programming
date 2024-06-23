55e6d5a6445dabe7f2d7044cd5d90b15be7b204b
Yadamzer <yadamzerterefe09@gmail.com>
2024-06-23 00:31:38 -0700

    Task 1

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""a script that lists all states
-with a name starting with N (upper N)
-from the database hbtn_0e_0_usa"""
+"""a script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa 
+where name matches the argument."""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    " script that lists all states with a name starting with N"
+    "script that takes in an argument and displays all values"
     db = MySQLdb.connect(
         host="localhost",
         user=sys.argv[1],
@@ -17,13 +17,13 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    query = "SELECT * FROM states WHERE name='{:s}'".format(sys.argv[4])
+    print(query)
+    cursor.execute(query)
 
     result = cursor.fetchall()
-
     for row in result:
         print(row)
 
-    cursor.close()
     db.close()
+    cursor.close()
