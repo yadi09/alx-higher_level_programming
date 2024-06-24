6255434c1bfe40a035d3998a3a0098f346cec038
Yadamzer <yadamzerterefe09@gmail.com>
2024-06-24 03:21:53 -0700

    Task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,13 +2,15 @@
 """a script that lists all State objects
 from the database hbtn_0e_6_usa"""
 
+import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://yadi09:Yadi_0988@localhost:3306/hbtn_0e_6_usa"
+    engine = create_engine("\
+mysql+mysqldb://{}:{}@localhost:3306/{}\
+".format(sys.argv[1], sys.argv[2], sys.argv[3])
     )
 
     Base.metadata.create_all(engine)
