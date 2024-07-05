ddb945ebd4494ed22ad3b2074fb8845d0ba7204a
Yadamzer <yadamzerterefe09@gmail.com>
2024-07-05 02:24:15 -0700

    Task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,12 +5,12 @@
 def find_peak(list_of_integers):
     "finds a peak"
     li = list_of_integers
-    l = len(li)
-    if l == 0:
+    lng = len(li)
+    if lng == 0:
         return
-    m = l // 2
-    if (m == l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
+    m = lng // 2
+    if (m == lng - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
         return li[m]
-    if m != l - 1 and li[m + 1] > li[m]:
+    if m != lng - 1 and li[m + 1] > li[m]:
         return find_peak(li[m + 1:])
     return find_peak(li[:m])
