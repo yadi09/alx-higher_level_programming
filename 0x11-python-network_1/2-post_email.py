cd84a59a817a8a0b8874266a5f25e6ba814b805c
Yadamzer <yadamzerterefe09@gmail.com>
2024-07-05 18:21:28 -0700

    Task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""script that takes in a URL and an email
+sends a POST request to the passed URL
+with the email as a parameter"""
+from urllib import request, parse
+import sys
+
+if __name__ == "__main__":
+    query_str = parse.urlencode({'email': sys.argv[2]}).encode('utf-8')
+    url = sys.argv[1]
+    requ = request.Request(url, query_str, method='POST')
+    with request.urlopen(requ) as respo:
+        data = respo.read().decode(utf-8)
+        print(data)
