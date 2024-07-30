a2c3bef6790783c0595e58e94e25cab483c2aaf3
Yadamzer <yadamzerterefe09@gmail.com>
2024-07-30 14:53:08 -0700

    Task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
-    if (err) {
-	console.log(err);
-    }
+  if (err) {
+    console.log(err);
+  }
 });
