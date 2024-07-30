15b7a1de2c82d8353f154e65198e3e6c6b300db7
Yadamzer <yadamzerterefe09@gmail.com>
2024-07-30 15:21:05 -0700

    Task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  }
+  const filmData = JSON.parse(body);
+  console.log(filmData.title);
+});
