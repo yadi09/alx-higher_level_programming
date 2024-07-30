d54eafc8945bf51f3c7964222531268ea9221e5e
Yadamzer <yadamzerterefe09@gmail.com>
2024-07-30 15:38:33 -0700

    Task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,6 +4,9 @@ const request = require('request');
 const fs = require('fs');
 
 request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+  }
   fs.writeFile(process.argv[3], body, 'utf8', function (error) {
     if (error) {
       console.error(error);
